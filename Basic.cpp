62945bcc8901259d141b74b32be100a5e79a5a72
mazharul haque Leon <mazharhaqueaiub@gmail.com>
2025-07-31 23:56:37 +0600

    Refactor Basic.cpp and add executable files

--- Basic.cpp
+++ Basic.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
+/*#include<iostream>
+#define PI 3.14
 using namespace std;
 int main(){
     cout<<"Hello, World!"<<endl;
+    cout<<PI;
     return 0; 
+} */
+
+#include<iostream>
+using namespace std;
+
+
+int main (){ 
+
+/*
+    cout<<"****\n";
+    cout<<"***\n";
+    cout<<"**\n";
+    cout<<"*";
+    /*cout<<"****\n***\n**\n*"; 
+
+      int a=5;
+
+      int b=10;
+      cout<<"a is:"<<a<<endl;
+        cout<<"b is:"<<b<<endl; */
+
+        int science;
+        int math
+
+
+
+    return 0;
 }
